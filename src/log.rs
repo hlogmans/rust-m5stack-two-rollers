d87aac72b331997b425dd3d9b55b88e80f168a42
Hugo Logmans <hugo.logmans@alten.nl>
2025-12-26 11:09:34 +0100

    Text output repaired, integrating defmt fully is now for later

--- src/log.rs
+++ src/log.rs
@@ -1,27 +1,34 @@
-//! Logging macros backed by `defmt` so output decodes and colors correctly in the monitor.
-//!
-//! Note: format strings must follow `defmt` formatting rules (implementing `Format`).
+//! Simple logging macros using esp_println
 
-/// Log an info message via `defmt::info!`.
+/// Log an info message
 #[macro_export]
 macro_rules! info {
     ($($arg:tt)*) => {
-        defmt::info!($($arg)*);
+        $crate::__print_internal!("[INFO] {}", format_args!($($arg)*))
     };
 }
 
-/// Log a warning message via `defmt::warn!`.
+/// Log a warning message
 #[macro_export]
 macro_rules! warn {
     ($($arg:tt)*) => {
-        defmt::warn!($($arg)*);
+        $crate::__print_internal!("[WARN] {}", format_args!($($arg)*))
     };
 }
 
-/// Log a debug message via `defmt::debug!`.
+/// Log a debug message
 #[macro_export]
 macro_rules! debug {
     ($($arg:tt)*) => {
-        defmt::debug!($($arg)*);
+        $crate::__print_internal!("[DEBUG] {}", format_args!($($arg)*))
+    };
+}
+
+// Internal macro for actual printing - uses esp_println
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __print_internal {
+    ($fmt:expr, $arg:expr) => {
+        esp_println::println!($fmt, $arg)
     };
 }
